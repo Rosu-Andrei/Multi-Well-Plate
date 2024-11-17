62b9b38f0e07d56af265fe541e671cdd85ef5212
Rosu Andrei <andrei_rosu1986@yahoo.com>
2024-11-17 19:44:47 +0200

    refactor - added the web worker for the selection process + Subject (an observable).

--- src/app/multi-well-plate/multi-well-plate.component.ts
+++ src/app/multi-well-plate/multi-well-plate.component.ts
@@ -1,17 +1,20 @@
-import {Component} from '@angular/core';
-import {faBars, faFlask, faSearchMinus, faSearchPlus} from '@fortawesome/free-solid-svg-icons';
-import {mockWells, Well} from "../model/well";
-import {PlateService} from "../services/plate.service";
-import {WellSelectionService} from "../services/well-selection.service";
-import {load} from "@angular-devkit/build-angular/src/utils/server-rendering/esm-in-memory-loader/loader-hooks";
+import {Component, OnInit} from '@angular/core';
+import {
+  faBars,
+  faFlask,
+  faSearchMinus,
+  faSearchPlus,
+} from '@fortawesome/free-solid-svg-icons';
+import {mockWells, Well} from '../model/well';
+import {PlateService} from '../services/plate.service';
+import {WellSelectionService} from '../services/well-selection.service';
 
 @Component({
   selector: 'app-multi-well-plate',
   templateUrl: './multi-well-plate.component.html',
-  styleUrls: ['./multi-well-plate.component.css']
+  styleUrls: ['./multi-well-plate.component.css'],
 })
-export class MultiWellPlateComponent {
-
+export class MultiWellPlateComponent implements OnInit {
   faFlask = faFlask;
   faSearchPlus = faSearchPlus;
   faSearchMinus = faSearchMinus;
@@ -23,13 +26,27 @@ export class MultiWellPlateComponent {
   sampleId: string = ''; // Default sample ID
   sampleRole: string = 'Unknown Sample'; // Default sample role
   currentWell: Well | null = null; // Currently selected single well
-  selectedWellsPositions: string = ''; // used for displaying multiple wells selected their ids
+  selectedWellsPositions: string = ''; // IDs of selected wells
 
   baseCellSize: number = 30; // Base size for cells in pixels
 
-  constructor(public plateService: PlateService,
-              public selectionService: WellSelectionService) {
 
+  constructor(
+    public plateService: PlateService,
+    public selectionService: WellSelectionService
+  ) {
+  }
+
+  ngOnInit(): void {
+    /**
+     * we subscribe to the Subject so that we receive the data that it emits.
+     */
+    this.selectionService.selectionChangeSubject.subscribe(
+      (selectedWells: Well[]) => {
+        this.updateCurrentWellPosition();
+        this.updateSampleInfo();
+      }
+    );
   }
 
   get cellSize(): number {
@@ -37,19 +54,18 @@ export class MultiWellPlateComponent {
   }
 
   selectPlate(plateSize: number | undefined): void {
-    if (plateSize != 96 && plateSize != 384) {
+    if (plateSize !== 96 && plateSize !== 384) {
       console.error('Unsupported plate size:', plateSize);
       return;
     }
     this.plateService.setupPlate(plateSize);
     this.selectionService.clearSelection();
-
+    this.selectionService.initializeWorker(); // Re-initialize worker with new wells
   }
 
-
   load(): void {
-    this.selectionService.selection.clear();
-    mockWells.forEach(well => {
+    this.selectionService.clearSelection();
+    mockWells.forEach((well) => {
       const columnChar = well.id.charAt(0);
       const rowStr = well.id.slice(1);
 
@@ -72,48 +88,40 @@ export class MultiWellPlateComponent {
         );
         return;
       }
-      this.selectionService.selection.select(this.plateService.getWells()[rowIndex][columnIndex]);
+      const selectedWell = this.plateService.getWells()[rowIndex][columnIndex];
+      // Simulate a selection event
+      const event = {
+        ctrlKey: true,
+        metaKey: true,
+        shiftKey: false,
+      } as MouseEvent;
+      this.selectionService.toggleWellSelection(event, selectedWell);
     });
   }
 
-
   toggleWellSelection(event: MouseEvent, well: Well): void {
     this.selectionService.toggleWellSelection(event, well);
-    /**
-     * called to update the current well position in the well settings tab
-     */
-    this.updateCurrentWellPosition();
-    /**
-     * called to update the current well sampleId and sampleRole in the well settings tab.
-     */
-    this.updateSampleInfo();
   }
-
 
   toggleRowSelection(event: MouseEvent, rowIndex: number): void {
     this.selectionService.toggleRowSelection(event, rowIndex);
-    this.updateCurrentWellPosition();
-    this.updateSampleInfo();
   }
 
   toggleColumnSelection(event: MouseEvent, columnIndex: number): void {
     this.selectionService.toggleColumnSelection(event, columnIndex);
-    this.updateCurrentWellPosition();
-    this.updateSampleInfo();
   }
 
-
   zoomIn(): void {
-    if (this.zoomLevel < 3) { // Maximum zoom level set to 3x
+    if (this.zoomLevel < 3) {
       this.zoomLevel += 0.1;
-      this.zoomLevel = Math.round(this.zoomLevel * 10) / 10; // Round to one decimal
+      this.zoomLevel = Math.round(this.zoomLevel * 10) / 10;
     }
   }
 
   zoomOut(): void {
-    if (this.zoomLevel > 0.5) { // Minimum zoom level set to 0.5x
+    if (this.zoomLevel > 0.5) {
       this.zoomLevel = Math.max(0.5, this.zoomLevel - 0.1);
-      this.zoomLevel = Math.round(this.zoomLevel * 10) / 10; // Round to one decimal
+      this.zoomLevel = Math.round(this.zoomLevel * 10) / 10;
     }
   }
 
@@ -125,75 +133,52 @@ export class MultiWellPlateComponent {
     this.activeTab = tab;
   }
 
-  /**
-   * this method determines if only a single well has been selected. If it is so,
-   * then the current well will point to this well. If multiple wells are selected or none, then
-   * the current well will receive the null value.
-   *
-   * This method is essential in showing the current selected well position in the readOnly box
-   */
   updateCurrentWellPosition(): void {
     const selection = this.selectionService.selection;
-    if (selection.selected.length == 1) {
+    if (selection.selected.length === 1) {
       this.currentWell = selection.selected[0];
       this.selectedWellsPositions = this.currentWell.id;
     } else if (selection.selected.length > 1) {
       this.currentWell = null;
-      this.selectedWellsPositions = selection.selected.map((well) => well.id).join(' ');
+      this.selectedWellsPositions = selection.selected
+        .map((well) => well.id)
+        .join(' ');
     } else {
       this.currentWell = null;
       this.selectedWellsPositions = '';
     }
   }
 
-  /**
-   * This method is very similar in what it does with the one above. The difference is that,
-   * it displays the sampleID and sampleRole that a selected well has.
-   *
-   * If multiple wells are selected, the window will display "" as the id and Unknown Value as the sample role.
-   */
   updateSampleInfo(): void {
-    const selection = this.selectionService.selection;
+    const array = this.selectionService.selection.selected;
     if (this.currentWell) {
       this.sampleId = this.currentWell.sampleId || '';
       this.sampleRole = this.currentWell.sampleRole || 'Unknown Sample';
     } else {
       this.sampleId = '';
       this.sampleRole = 'Unknown Sample';
-      selection.selected.forEach((well) => {
+      array.forEach((well) => {
         if (well.sampleId) {
           this.sampleId += well.sampleId + ' ';
         }
       });
-      if (this.sampleId == '') {
+      if (this.sampleId.trim() === '') {
         this.sampleId = 'No sample ID has been entered';
       }
     }
   }
 
-  /**
-   * When the user enters a sampleId, this method will:
-   * 1. update the component sampleId (because we want to show it updated in the browser)
-   * 2. Each selected well will receive the sampleId entered. (if only one is selected,
-   * the only one will receive it)
-   */
   onSampleIdChange(newSampleId: string): void {
     this.sampleId = newSampleId;
-    this.selectionService.selection.selected.forEach(well => {
+    this.selectionService.selection.selected.forEach((well) => {
       well.sampleId = newSampleId;
     });
   }
 
-  /**
-   * This method is very similar with the one above, the only difference being that
-   * it is updating the Sample Role property.
-   */
   onSampleRoleChange(newSampleRole: string): void {
     this.sampleRole = newSampleRole;
-    this.selectionService.selection.selected.forEach(well => {
+    this.selectionService.selection.selected.forEach((well) => {
       well.sampleRole = newSampleRole;
     });
   }
-
-  //protected readonly load = load;
 }
--- src/app/services/well-selection.service.ts
+++ src/app/services/well-selection.service.ts
@@ -1,94 +1,132 @@
 import {Injectable} from '@angular/core';
-import {SelectionModel} from '@angular/cdk/collections';
 import {PlateService} from './plate.service';
-import {Well} from "../model/well";
+import {Well} from '../model/well';
+import {SelectionModel} from '@angular/cdk/collections';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+/**
+ * The WellSelectionService acts as a bridge between the Web Worker and the Angular component, the multi-well.ts file.
+ * It manages the selection state using the SelectionModel and communicates with the worker by using messages.
+ */
 export class WellSelectionService {
   selection = new SelectionModel<Well>(true, []);
-  lastSelectedWell: Well | null = null;
+  private worker!: Worker;
+
+  /**
+   * Subject is type of Observable that can receive data and emit data to which then we subscribe to.
+   * This specific subject will emit an array of Well. This will notify any subscriber whenever a selection
+   * of wells has changed
+   */
+  public selectionChangeSubject = new Subject<Well[]>();
 
   constructor(private plateService: PlateService) {
-  }
+    if (typeof Worker !== 'undefined') {
+      // Create a new web worker
+      this.worker = new Worker(
+        new URL('../well-selection.worker.ts', import.meta.url),
+        {type: 'module'}
+      );
+      /**
+       * the "onmessage" method is the one that will return the data from the worker, after the worker
+       * has done all its job. Think of it as Future Promise.
+       */
+      this.worker.onmessage = ({data}) => {
 
-  toggleWellSelection(event: MouseEvent, well: Well): void {
-    const ctrlPressed = event.ctrlKey || event.metaKey;
-    const shiftPressed = event.shiftKey;
-
-    if (ctrlPressed && shiftPressed && this.lastSelectedWell) {
-      const newSelection = this.getWellsInRange(this.lastSelectedWell, well);
-      this.selection.select(...newSelection);
-    } else if (ctrlPressed) {
-      this.selection.toggle(well);
-      this.lastSelectedWell = well;
+        const message = data;
+        /**
+         * if the message type is equal to "selectionUpdate", this means
+         * that a selection update has happened.
+         * The payload that is coming back from the web worker is the Set with the well ids that
+         * are up for selection
+         */
+        if (message.type === 'selectionUpdate') {
+          this.updateSelectionModel(message.payload);
+        }
+      };
+      /**
+       * the main thread sends to the worker an "initialize" message that will
+       * set up the well matrix for it.
+       */
+      this.initializeWorker();
     } else {
-      this.selection.clear();
-      this.selection.select(well);
-      this.lastSelectedWell = well;
+      console.error('Web Workers are not supported in this environment.');
     }
   }
 
-  getWellsInRange(startWell: Well, endWell: Well): Well[] {
-    const startRow = startWell.row!;
-    const endRow = endWell.row!;
-    const startCol = startWell.column!;
-    const endCol = endWell.column!;
+  /**
+   * send the well matrix to the web worker.
+   */
+  initializeWorker(): void {
+    this.worker.postMessage({
+      type: 'initialize',
+      payload: {wells: this.plateService.getWells()},
+    });
+  }
 
-    const minRow = Math.min(startRow, endRow);
-    const maxRow = Math.max(startRow, endRow);
-    const minCol = Math.min(startCol, endCol);
-    const maxCol = Math.max(startCol, endCol);
-
-    const wellsInRange: Well[] = [];
-    const wells = this.plateService.getWells();
-
-    for (let row = minRow; row <= maxRow; row++) {
-      for (let col = minCol; col <= maxCol; col++) {
-        wellsInRange.push(wells[row][col]);
-      }
-    }
-    return wellsInRange;
+  /**
+   * constructs a payload using the parameters and then sends it to the worker with
+   * the message type "toggleWellSelection" so that the message worker will go in the case
+   * for a specific well.
+   */
+  toggleWellSelection(event: MouseEvent, well: Well): void {
+    const payload = {
+      ctrlPressed: event.ctrlKey || event.metaKey,
+      shiftPressed: event.shiftKey,
+      well: well,
+    };
+    this.worker.postMessage({type: 'toggleWellSelection', payload});
   }
 
   toggleRowSelection(event: MouseEvent, rowIndex: number): void {
     const ctrlPressed = event.ctrlKey || event.metaKey;
-    const rowWells = this.plateService.getWells()[rowIndex];
-
-    if (ctrlPressed) {
-      const allSelected = rowWells.every((well) => this.selection.isSelected(well));
-      if (allSelected) {
-        this.selection.deselect(...rowWells);
-      } else {
-        this.selection.select(...rowWells);
-      }
-    } else {
-      this.selection.clear();
-      this.selection.select(...rowWells);
-    }
+    this.worker.postMessage({
+      type: 'toggleRowSelection',
+      payload: {ctrlPressed, rowIndex},
+    });
   }
 
   toggleColumnSelection(event: MouseEvent, columnIndex: number): void {
     const ctrlPressed = event.ctrlKey || event.metaKey;
-    const wells = this.plateService.getWells();
-    const colWells = wells.map((row) => row[columnIndex]);
-
-    if (ctrlPressed) {
-      const allSelected = colWells.every((well) => this.selection.isSelected(well));
-      if (allSelected) {
-        this.selection.deselect(...colWells);
-      } else {
-        this.selection.select(...colWells);
-      }
-    } else {
-      this.selection.clear();
-      this.selection.select(...colWells);
-    }
+    this.worker.postMessage({
+      type: 'toggleColumnSelection',
+      payload: {ctrlPressed, columnIndex},
+    });
   }
 
+  /**
+   * Sends a "clearSelection" message to the worker to reset all selections.
+   */
   clearSelection(): void {
+    this.worker.postMessage({type: 'clearSelection'});
+  }
+
+  private updateSelectionModel(selectedWellIds: string[]): void {
+    /**
+     * we create a well array that will contain only the wells that have been marked by the web
+     * worker for selection. The identification is done using the Set<> from the web worker that contains the well ids.
+     */
+    const selectedWells = this.plateService.getWells().flat().filter((well) => {
+        return selectedWellIds.includes(well.id);
+      }
+    );
+    /**
+     * First we clear all the selected wells from the SelectionModule object, and then we tell it to select only
+     * the wells that are in the newly created array.
+     */
     this.selection.clear();
-    this.lastSelectedWell = null;
+    this.selection.select(...selectedWells);
+
+    /**
+     * Emits the updated selection via selectionChangeSubject for any subscribed components. In this way, the
+     * changes are communicated to the components.
+     */
+    this.selectionChangeSubject.next(selectedWells);
+  }
+
+  isSelected(well: Well): boolean {
+    return this.selection.isSelected(well);
   }
 }
