14dfc6ef270680eb0ab3d5ac7dc9e3ee44ec06cb
Rosu Andrei <andrei_rosu1986@yahoo.com>
2024-11-21 15:24:06 +0200

    table-adding - added comments to all the store files.

--- src/app/store/well.reducer.ts
+++ src/app/store/well.reducer.ts
@@ -2,11 +2,21 @@ import {createReducer, on} from '@ngrx/store';
 import {updateWellSample} from './well.action';
 import {initialState} from "./well.state";
 
-
+/**
+ * the reducer listens for the actions defined, in this case the only actions defined and
+ * that it listens is "updateWellSample".
+ *
+ * initially, the reducer has the initial state of the store, represented by the first argument "initialState".
+ * one an action of type "updateWellSample" has happened.
+ *
+ * The "state" variable represent the current state of a well. When a well receives changes in either sampleId or sampleRole,
+ * the reducer will take the current state of that well (does this using the id) and returns a new sample
+ * for that well with the new changes that have been made. Because we return a new state, this means we have immutability.
+ */
 export const wellSamplesReducer = createReducer(
   initialState,
   on(updateWellSample, (state, {wellId, changes}) => {
-    const existingSample = state.samples[wellId] || {};
+    const existingSample = state.samples[wellId] || {}; // retrieve the current sampleId and sampleRole of the well identified by its id
     return {
       ...state,
       samples: {
--- src/app/store/well.state.ts
+++ src/app/store/well.state.ts
@@ -1,12 +1,22 @@
+/**
+ * structure for the sampleId and sampleRole
+ */
 export interface WellSample {
   sampleId?: string,
   sampleRole?: string
 }
 
+/**
+ * this represents a sample in the state store. The key is the wellId, whose values represent
+ * the sampleId and sampleRole for that respective well
+ */
 export interface WellSamplesState {
   samples: Record<string, WellSample>
 }
 
+/**
+ * a constant that represents the initial state, when the application first launches.
+ */
 export const initialState: WellSamplesState = {
   samples: {},
 };
